e0bfcc6ecdff3a5d0d457e93e63ab804340b2162
刘兆明 <liuzhaoming@qianmi.com>
2015-11-18 18:28:34 +0800

    修改ES集群地址

--- search_platform/settings.py
+++ search_platform/settings.py
@@ -310,7 +310,7 @@ REST_FRAMEWORK = {
 SERVICE_BASE_CONFIG = {
     'meta_file': '/common/config.json',
     'redis': 'redis://172.17.8.253:6379/2',
-    'elasticsearch': 'http://192.168.65.131:9200,http://192.168.65.132:9200,http://192.168.65.130:9200',
+    'elasticsearch': 'http://192.168.65.167:9200,http://192.168.65.175:9200,http://192.168.65.231:9200',
     'meta_es_index': 'sp_search_platform_cfg',
     'meta_es_type': 'config',
     'meta_es_id': 'config_data',
