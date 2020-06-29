a0013ff60d2d84da2b99917c9d0ea0c726131e51
侯锐 <hourui@qianmi.com>
2020-06-29 14:47:27 +0800

    格式化exceptions.py的代码

--- common/exceptions.py
+++ common/exceptions.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import time
+
 __author__ = 'liuzhaoming'
 
 ERROR_INFO = {'GenericError': {'code': 900, 'message': 'Generic exception'},
@@ -97,14 +98,18 @@ class EsBulkOperationError(SearchPlatformException):
     """
     ES bulk操作异常
     """
+
     def __init__(self, bulk_result):
         self.bulk_result = bulk_result
         error_msg = ERROR_INFO['EsBulkOperationError']['message']
         SearchPlatformException.__init__(self, ERROR_INFO['EsBulkOperationError']['code'], error_msg)
+
+
 class MsgQueueFullError(SearchPlatformException):
     """
     MQ队列已满
     """
+
     def __init__(self, admin_id, capacity, is_vip=True):
         SearchPlatformException.__init__(self, ERROR_INFO['MsgQueueFullError']['code'],
                                          ERROR_INFO['MsgQueueFullError']['message'])
@@ -112,25 +117,33 @@ class MsgQueueFullError(SearchPlatformException):
         self._capacity = capacity
         self._is_vip = is_vip
         self.is_threshold_alarm = True
+
     @property
     def admin_id(self):
         return self._admin_id
+
     @property
     def capacity(self):
         return self._capacity
+
     @property
     def is_vip(self):
         return self._is_vip
+
     def __str__(self):
         return '{0}(AdminID:{1}, Vip:{2}, Capacity:{3}, {4})'.format(str(self.__class__.__name__), self.admin_id,
                                                                      self.is_vip, self.capacity, self.error)
+
     def __unicode__(self):
         return '{0}(AdminID:{1}, Vip:{2}, Capacity:{3}, {4})'.format(str(self.__class__.__name__), self.admin_id,
                                                                      self.is_vip, self.capacity, self.error)
+
+
 class RedoMsgQueueFullError(SearchPlatformException):
     """
     重做消息队列已满
     """
+
     def __init__(self, admin_id, capacity, is_vip=True):
         SearchPlatformException.__init__(self, ERROR_INFO['RedoMsgQueueFullError']['code'],
                                          ERROR_INFO['RedoMsgQueueFullError']['message'])
@@ -138,37 +151,50 @@ class RedoMsgQueueFullError(SearchPlatformException):
         self._capacity = capacity
         self._is_vip = is_vip
         self.is_threshold_alarm = True
+
     @property
     def admin_id(self):
         return self._admin_id
+
     @property
     def capacity(self):
         return self._capacity
+
     @property
     def is_vip(self):
         return self._is_vip
+
     def __str__(self):
         return '{0}(AdminID:{1}, Vip:{2}, Capacity:{3}, {4})'.format(str(self.__class__.__name__), self.admin_id,
                                                                      self.is_vip, self.capacity, self.error)
+
     def __unicode__(self):
         return '{0}(AdminID:{1}, Vip:{2}, Capacity:{3}, {4})'.format(str(self.__class__.__name__), self.admin_id,
                                                                      self.is_vip, self.capacity, self.error)
+
+
 class FinalFailMsgQueueFullError(SearchPlatformException):
     """
     最终失败消息队列已满
     """
+
     def __init__(self, capacity):
         SearchPlatformException.__init__(self, ERROR_INFO['FinalFailMsgQueueFullError']['code'],
                                          ERROR_INFO['FinalFailMsgQueueFullError']['message'])
         self._capacity = capacity
         self.is_threshold_alarm = True
+
     @property
     def capacity(self):
         return self._capacity
+
     def __str__(self):
         return '{0}(Capacity:{1}, {2})'.format(str(self.__class__.__name__), self.capacity, self.error)
+
     def __unicode__(self):
         return '{0}(Capacity:{1}, {2})'.format(str(self.__class__.__name__), self.capacity, self.error)
+
+
 class MsgHandlingFailError(SearchPlatformException):
     """
     MQ 消息处理失败异常
@@ -178,26 +204,34 @@ class MsgHandlingFailError(SearchPlatformException):
     ES_READ_TIMEOUT = 3
     ES_ERROR = 4
     PROCESS_ERROR = 5
+
     def __init__(self, source):
         SearchPlatformException.__init__(self, ERROR_INFO['MsgHandlingFailError']['code'],
                                          ERROR_INFO['MsgHandlingFailError']['message'])
         self._source = source
         self._event_time = time.time()
+
     @property
     def source(self):
         return self._source
+
     @property
     def event_time(self):
         return self._event_time
+
     @property
     def json(self):
         return {"source": self.source, "event_time": self.event_time}
+
     def __str__(self):
         return '{0}({1}, {2}, source={3}, event_time={4})'.format(str(self.__class__.__name__), self.error_code,
                                                                   self.error, self.source, self.event_time)
+
     def __unicode__(self):
         return '{0}({1}, {2}, source={3}, event_time={4})'.format(str(self.__class__.__name__), self.error_code,
                                                                   self.error, self.source, self.event_time)
+
+
 if __name__ == '__main__':
     error = InvalidParamError()
     print error
